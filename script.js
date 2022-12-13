e5ac57ca49dea6330fde6bfad7d8bc376a1900be
EddieHooks36 <eddiehooks36@gmail.com>
2022-12-12 21:39:35 -0800

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+let dbNumber = 4;
+
+function calculate(){
+    
+    
+
+
+
+
+
+
+
+
+}
+
+let firstNumber = prompt("Type the number four");
+let secondNumber = prompt("Type the number six"); 
+
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
