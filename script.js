1a36ab1f9fb30c41e24cea5e83f84b06108f7817
EddieHooks36 <eddiehooks36@gmail.com>
2022-12-13 16:52:11 -0800

    update

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-let result;
+function calculator(){
+    let operator = prompt('Enter operator ( either +, -, * or / ):');
 
-let operator = prompt('Enter operator ( either +, -, * or / ): ');
+    let number1 = prompt('Enter first number');
+    let number2 = prompt('Enter second number');
 
-let number1 = parseFloat(prompt('Enter first number: '));
-let number2 = parseFloat(prompt('Enter second number: '));
+    let result;
 
-switch(operator) {
-    case '+':
-         result = number1 + number2;
-        console.log(`${number1} + ${number2} = ${result}`);
-        break;
 
-    case '-':
-         result = number1 - number2;
-        console.log(`${number1} - ${number2} = ${result}`);
-        break;
-
-    case '*':
-         result = number1 * number2;
-        console.log(`${number1} * ${number2} = ${result}`);
-        break;
-
-    case '/':
-         result = number1 / number2;
-        console.log(`${number1} / ${number2} = ${result}`);
-        break;
-
-    default:
-        console.log('Invalid operator');
-        break;
+    if (operator == '+'){
+        result = number1 + number2;
 }
+    else if (operator == '-'){
+        result = number1 - number2;
+}
+    else if (operator == '*'){
+        result = number1 * number2;
+}
+    else {
+        result = number1 / number2;
+        document.getElementById("+, -, *, /"). innerHTML = `<p class="alert-error">Invalid Input</p>`;
+}
+}
+    
+    
+
